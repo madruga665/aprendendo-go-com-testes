a140af947310f5873239dd9965f32e9188a1264d
madruga665 <madruga665@hotmail.com>
2026-05-01 22:45:39 -0300

    feat: add more languages

--- hello.go
+++ hello.go
@@ -2,16 +2,26 @@ package main
 
 import "fmt"
 
-func hello(name string) string {
+func hello(name string, language string) string {
 	const prefixMessage = "Hello, "
+	const portuguesePrefixMessage = "Olá, "
+	const spanishPrefixMessage = "Hola, "
 
 	if name == "" {
 		name = "world"
+	}
+
+	if language == "portuguese" {
+		return portuguesePrefixMessage + name
+	}
+
+	if language == "spanish" {
+		return spanishPrefixMessage + name
 	}
 
 	return prefixMessage + name
 }
 
 func main() {
-	fmt.Println(hello("madruga665"))
+	fmt.Println(hello("madruga665", "portuguese"))
 }
--- hello_test.go
+++ hello_test.go
@@ -10,15 +10,29 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("Say hello for people", func(t *testing.T) {
-		result := hello("madruga665")
+		result := hello("madruga665", "english")
 		expected := "Hello, madruga665"
 
 		verifyMessage(t, result, expected)
 	})
 
 	t.Run("Say hello World when name is empty string", func(t *testing.T) {
-		result := hello("")
+		result := hello("", "english")
 		expected := "Hello, world"
+
+		verifyMessage(t, result, expected)
+	})
+
+	t.Run("Say hello in portuguese when languange is portuguese", func(t *testing.T) {
+		result := hello("Dante", "portuguese")
+		expected := "Olá, Dante"
+
+		verifyMessage(t, result, expected)
+	})
+
+	t.Run("Say hello in spanish when languange is spanish", func(t *testing.T) {
+		result := hello("Dante", "spanish")
+		expected := "Hola, Dante"
 
 		verifyMessage(t, result, expected)
 	})
