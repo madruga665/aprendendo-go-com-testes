84ba2ea22481fa20ba278226117ce6bc23bdf153
madruga665 <madruga665@hotmail.com>
2026-05-08 23:05:21 -0300

    feat: add sumAllRest

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -9,3 +9,19 @@ func sum(numbers []int) int {
 	}
 	return result
 }
+
+func sumAllRest(numbers ...[]int) []int {
+	var result []int
+
+	for _, number := range numbers {
+		if len(number) == 0 {
+			result = append(result, 0)
+		} else {
+
+			final := number[1:]
+			result = append(result, sum(final))
+		}
+	}
+
+	return result
+}
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("array 6 numbers", func(t *testing.T) {
@@ -23,4 +26,24 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAllRest(t *testing.T) {
+	t.Run(" sum slices", func(t *testing.T) {
+		result := sumAllRest([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
+
+	t.Run(" sum empty slices", func(t *testing.T) {
+		result := sumAllRest([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
 }
